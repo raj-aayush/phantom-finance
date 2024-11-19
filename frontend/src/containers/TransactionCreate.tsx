95cc29a4bd43b7f8b93289052bfc4e157cf445bb
Aayush Raj <aayush616@gmail.com>
2024-11-18 18:22:31 -0800

    Enable transaction creation

--- frontend/src/containers/TransactionCreate.tsx
+++ frontend/src/containers/TransactionCreate.tsx
@@ -2,6 +2,15 @@ import {Button, Card, Form, Input, Layout, Select} from "antd";
 import axios from "axios";
 import SideMenuWrapper from "./SideMenuWrapper.tsx";
 
+const transactionAccountValidator = (form) => {
+    let sender = form.getFieldValue('sender');
+    let receiver = form.getFieldValue('receiver');
+    if(sender && receiver && sender.toLowerCase() == receiver.toLowerCase()) {
+        return Promise.reject("Sender cannot be the same as receiver");
+    }
+    return Promise.resolve();
+}
+
 const TransactionCreate = () => {
     const [form] = Form.useForm();
     return (
@@ -9,14 +18,14 @@ const TransactionCreate = () => {
             <Layout.Content>
                 <Card>
                     <h2>New Transaction</h2>
-                    <Form form={form} labelCol={{span: 6}} validateTrigger="onBlur" onFinish={(values) => {
+                    <Form form={form} labelCol={{span: 6}} onFinish={(values) => {
                         axios.post('/api/transactions/', values)
                     }}>
-                        <Form.Item name="sender" label="Sender" rules={[{required: true}]}>
-                            <Select></Select>
+                        <Form.Item name="sender" label="Sender" rules={[{required: true, validator: () => transactionAccountValidator(form)}]}>
+                            <Input />
                         </Form.Item>
-                        <Form.Item name="receiver" label="Receiver" rules={[{required: true}]}>
-                            <Select></Select>
+                        <Form.Item name="receiver" label="Receiver" rules={[{required: true, validator: () => transactionAccountValidator(form)}]}>
+                            <Input />
                         </Form.Item>
                         <Form.Item name="amount" label="Amount" normalize={value => parseInt(value, 10)} rules={[{required: true, type: "number", min: 5}]}>
                             <Input type="number"/>
